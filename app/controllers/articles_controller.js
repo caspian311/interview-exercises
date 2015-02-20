f6be6e8e3382dfa88ec9655c13412582ae24129a
Matt Todd <matt.todd@asynchrony.com>
2015-02-20 08:51:22 -0600

    fixed example... better usage of supertest ... removed views as they are not being used

--- app/controllers/articles_controller.js
+++ app/controllers/articles_controller.js
@@ -1,13 +1,15 @@
 var express = require('express'),
-  router = express.Router(),
-  db = require('../models');
+    router = express.Router(),
+    db = require('../models');
 
 module.exports = function (app) {
-  app.use('/', router);
+    app.use('/', router);
 };
 
-router.get('/service/articles', function (req, res) {
-  db.Article.findAll().success(function (articles) {
-    res.status(200).send(articles).end();
-  });
-});
+function allArticles(req, res) {
+    db.Article.findAll().success(function (articles) {
+        res.status(200).send(articles).end();
+    });
+}
+
+router.get('/service/articles', allArticles);
