2057398122b0a2de910f9e33144faa35e7a39e1f
Matt Todd <matt.todd@asynchrony.com>
2016-08-11 15:25:02 -0500

    rock paper scissors app

---
+++ test/rock_paper_scissors_test.js
@@ -0,0 +1,7 @@
+var fizz_buzz = require('../app/rock_paper_scissors');
+
+describe('rock paper scissors', function() {
+    it('should do something', function() {
+        expect(1).to.equal(1);
+    });
+});
